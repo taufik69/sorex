3b9c93f0a47b14f8eda8f1c6bebc6b73b3b07209
Taufik islam <taufik35-2915@diu.edu.bd>
2023-09-27 20:04:30 +0600

    navbar responsive done

--- src/Component/Header/Index.jsx
+++ src/Component/Header/Index.jsx
@@ -9,19 +9,22 @@ import { FaBars } from "react-icons/fa";
 import { AiOutlinePlus } from "react-icons/ai";
 const Header = () => {
   let [navbar, setnavbar] = useState(false);
+  let [drawernavbar, setdrawernavbar] = useState(true);
   useEffect(() => {
     function resizeBody() {
       if (window.innerWidth >= 900) {
         setnavbar(true);
+        setdrawernavbar(false);
       } else {
         setnavbar(false);
+        setdrawernavbar(true);
       }
     }
 
     window.addEventListener("resize", resizeBody);
     resizeBody();
   }, []);
-
+  console.log("navbar status : ", navbar);
   return (
     <div className={`mt-5 relative`}>
       <motion.div
@@ -281,182 +284,168 @@ const Header = () => {
             </div>
 
             {/* acordian down */}
+            {drawernavbar && (
+              <div id="accordion-collapse" data-accordion="collapse">
+                <h2 id="accordion-collapse-heading-1">
+                  <button
+                    type="button"
+                    class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 "
+                    data-accordion-target="#accordion-collapse-body-1"
+                    aria-expanded="false"
+                    aria-controls="accordion-collapse-body-1"
+                  >
+                    <span>What is Flowbite?</span>
+                    <svg
+                      data-accordion-icon
+                      class="w-3 h-3 rotate-180 shrink-0"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 10 6"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 5 5 1 1 5"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div
+                  id="accordion-collapse-body-1"
+                  class="hidden"
+                  aria-labelledby="accordion-collapse-heading-1"
+                >
+                  <div class="p-5 border border-b-0 border-gray-200 ">
+                    <p class="mb-2 text-gray-500 ">
+                      Flowbite is an open-source library of interactive
+                      components built on top of Tailwind CSS including buttons,
+                      dropdowns, modals, navbars, and more.
+                    </p>
+                  </div>
+                </div>
+                <h2 id="accordion-collapse-heading-2">
+                  <button
+                    type="button"
+                    class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500"
+                    data-accordion-target="#accordion-collapse-body-2"
+                    aria-expanded="false"
+                    aria-controls="accordion-collapse-body-2"
+                  >
+                    <span>Is there a Figma </span>
+                    <svg
+                      data-accordion-icon
+                      class="w-3 h-3 rotate-180 shrink-0"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 10 6"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 5 5 1 1 5"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div
+                  id="accordion-collapse-body-2"
+                  class="hidden"
+                  aria-labelledby="accordion-collapse-heading-2"
+                >
+                  <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">
+                      Flowbite is first conceptualized and designed using the
+                      Figma software so everything you see in the library has a
+                      design equivalent in our Figma file.
+                    </p>
+                  </div>
+                </div>
+                <h2 id="accordion-collapse-heading-3">
+                  <button
+                    type="button"
+                    class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500"
+                    data-accordion-target="#accordion-collapse-body-3"
+                    aria-expanded="false"
+                    aria-controls="accordion-collapse-body-3"
+                  >
+                    <span>What are the differences</span>
+                    <svg
+                      data-accordion-icon
+                      class="w-3 h-3 rotate-180 shrink-0"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 10 6"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 5 5 1 1 5"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div
+                  id="accordion-collapse-body-3"
+                  class="hidden"
+                  aria-labelledby="accordion-collapse-heading-3"
+                >
+                  <div class="p-5 border border-t-0 border-gray-200">
+                    <p class="mb-2 text-gray-500">
+                      The main difference is that the core components from Flo
+                    </p>
+                  </div>
+                </div>
 
-            <div id="accordion-collapse" data-accordion="collapse">
-              <h2 id="accordion-collapse-heading-1">
-                <button
-                  type="button"
-                  class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-                  data-accordion-target="#accordion-collapse-body-1"
-                  aria-expanded="true"
-                  aria-controls="accordion-collapse-body-1"
-                >
-                  <span>What is Flowbite?</span>
-                  <svg
-                    data-accordion-icon
-                    class="w-3 h-3 rotate-180 shrink-0"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
+                <h2 id="accordion-collapse-heading-4">
+                  <button
+                    type="button"
+                    class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500"
+                    data-accordion-target="#accordion-collapse-body-4"
+                    aria-expanded="false"
+                    aria-controls="accordion-collapse-body-4"
                   >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 5 5 1 1 5"
-                    />
-                  </svg>
-                </button>
-              </h2>
-              <div
-                id="accordion-collapse-body-1"
-                class="hidden"
-                aria-labelledby="accordion-collapse-heading-1"
-              >
-                <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                  <p class="mb-2 text-gray-500 dark:text-gray-400">
-                    Flowbite is an open-source library of interactive components
-                    built on top of Tailwind CSS including buttons, dropdowns,
-                    modals, navbars, and more.
-                  </p>
-                  <p class="text-gray-500 dark:text-gray-400">
-                    Check out this guide to learn how to{" "}
-                    <a
-                      href="/docs/getting-started/introduction/"
-                      class="text-blue-600 dark:text-blue-500 hover:underline"
+                    <span>What are the</span>
+                    <svg
+                      data-accordion-icon
+                      class="w-3 h-3 rotate-180 shrink-0"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 10 6"
                     >
-                      get started
-                    </a>{" "}
-                    and start developing websites even faster with components on
-                    top of Tailwind CSS.
-                  </p>
-                </div>
-              </div>
-              <h2 id="accordion-collapse-heading-2">
-                <button
-                  type="button"
-                  class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-                  data-accordion-target="#accordion-collapse-body-2"
-                  aria-expanded="false"
-                  aria-controls="accordion-collapse-body-2"
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 5 5 1 1 5"
+                      />
+                    </svg>
+                  </button>
+                </h2>
+                <div
+                  id="accordion-collapse-body-4"
+                  class="hidden"
+                  aria-labelledby="accordion-collapse-heading-4"
                 >
-                  <span>Is there a Figma file available?</span>
-                  <svg
-                    data-accordion-icon
-                    class="w-3 h-3 rotate-180 shrink-0"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 5 5 1 1 5"
-                    />
-                  </svg>
-                </button>
-              </h2>
-              <div
-                id="accordion-collapse-body-2"
-                class="hidden"
-                aria-labelledby="accordion-collapse-heading-2"
-              >
-                <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-                  <p class="mb-2 text-gray-500 dark:text-gray-400">
-                    Flowbite is first conceptualized and designed using the
-                    Figma software so everything you see in the library has a
-                    design equivalent in our Figma file.
-                  </p>
-                  <p class="text-gray-500 dark:text-gray-400">
-                    Check out the{" "}
-                    <a
-                      href="https://flowbite.com/figma/"
-                      class="text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      Figma design system
-                    </a>{" "}
-                    based on the utility classes from Tailwind CSS and
-                    components from Flowbite.
-                  </p>
+                  <div class="p-5 border border-t-0 border-gray-200 ">
+                    <p class="mb-2 text-gray-500 ">
+                      The main dis that the core components from Flowbitens of
+                      pages.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <h2 id="accordion-collapse-heading-3">
-                <button
-                  type="button"
-                  class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-                  data-accordion-target="#accordion-collapse-body-3"
-                  aria-expanded="false"
-                  aria-controls="accordion-collapse-body-3"
-                >
-                  <span>
-                    What are the differences between Flowbite and Tailwind UI?
-                  </span>
-                  <svg
-                    data-accordion-icon
-                    class="w-3 h-3 rotate-180 shrink-0"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M9 5 5 1 1 5"
-                    />
-                  </svg>
-                </button>
-              </h2>
-              <div
-                id="accordion-collapse-body-3"
-                class="hidden"
-                aria-labelledby="accordion-collapse-heading-3"
-              >
-                <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
-                  <p class="mb-2 text-gray-500 dark:text-gray-400">
-                    The main difference is that the core components from
-                    Flowbite are open source under the MIT license, whereas
-                    Tailwind UI is a paid product. Another difference is that
-                    Flowbite relies on smaller and standalone components,
-                    whereas Tailwind UI offers sections of pages.
-                  </p>
-                  <p class="mb-2 text-gray-500 dark:text-gray-400">
-                    However, we actually recommend using both Flowbite, Flowbite
-                    Pro, and even Tailwind UI as there is no technical reason
-                    stopping you from using the best of two worlds.
-                  </p>
-                  <p class="mb-2 text-gray-500 dark:text-gray-400">
-                    Learn more about these technologies:
-                  </p>
-                  <ul class="pl-5 text-gray-500 list-disc dark:text-gray-400">
-                    <li>
-                      <a
-                        href="https://flowbite.com/pro/"
-                        class="text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Flowbite Pro
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="https://tailwindui.com/"
-                        rel="nofollow"
-                        class="text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        Tailwind UI
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+            )}
 
             {/* acordian down */}
           </div>
